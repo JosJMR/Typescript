bfef9491d678706ac64102d5af1563303dcbde87
JosJMR <josiasj.mr72@gmail.com>
2019-10-31 16:17:58 -0600

    Practiced dataTypes in typescript.

---
+++ dataTypes.ts
@@ -0,0 +1,28 @@
+let str: string;
+let bool: boolean;
+let num: number;
+let stringArray: string[];
+let numArray: number[];
+let boolArray: boolean[];
+let tuple: [string, number, boolean];
+
+enum Colors {
+  blue = "#0000FF",
+  red = "#FF0000",
+  green = "#00FF00",
+  yellow = "#FFFF00",
+  white = "#FFFFFF"
+}
+
+str = "Hello";
+num = 11;
+bool = true;
+
+stringArray = [];
+stringArray[0] = "Hello";
+stringArray[1] = "world";
+stringArray[2] = "!";
+
+numArray = [0, 1, 2, 3, 4, 5];
+
+tuple = ["josias", 22, false];
